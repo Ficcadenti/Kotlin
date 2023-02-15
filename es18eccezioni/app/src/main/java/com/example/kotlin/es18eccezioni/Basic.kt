2df59db71757804b94a2733add7ec2e66d3576a1
Raffaele Ficcadenti <r.ficcadenti@exolab.app>
2023-02-15 12:23:26 +0100

    Eccezioni

--- es18eccezioni/app/src/main/java/com/example/kotlin/es18eccezioni/Basic.kt
+++ es18eccezioni/app/src/main/java/com/example/kotlin/es18eccezioni/Basic.kt
@@ -2,4 +2,32 @@ package com.example.kotlin.es18eccezioni
 
 fun main(args : Array<String>) {
     println("Test 18 - Eccezioni !!!")
+
+    var x:Int? = getNumber("10x")
+    println(x)
+
+    validate(1)
+
+
+}
+
+fun getNumber(str:String):Int?{
+    return try{
+        Integer.parseInt(str)
+    }catch(e:java.lang.NumberFormatException)
+    {
+        e.printStackTrace()
+        null
+    }
+    finally {
+        println("Codice sempre esguito !!!!!!")
+    }
+}
+
+fun validate(age:Int)
+{
+    if(age<18)
+        throw ArithmeticException("Eta inferiore ai 18 anni")
+    else
+        println("OK !!!!")
 }
